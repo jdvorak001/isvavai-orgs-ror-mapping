d5769aac1bfdca30b307384a4b53e7ad23e1d4a2
Jan Dvorak <jdvorak001@users.noreply.github.com>
2022-06-17 22:31:47 +0200

    Add the script to retrieve current records

---
+++ util/retrieve-orgs-data.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# This script retrieves the current ROR records of the ROR IDs listed in the organization.csv file from the ROR API, 
+# indents them and places them in the data/ directory
+#
+
+F=organizations.csv
+D=data
+
+mkdir -p "$D"/
+grep -oh -e ',https://ror\.org/0........,' $F \
+| sed -e 's/,https:\/\/ror\.org\//https:\/\/api.ror.org\/organizations\//' -e 's/,$//' \
+| wget -nv -P "$D"/ -i -
+
+for X in "$D"/0??????[0-9][0-9]
+do
+	python3 -m json.tool <"$X" >"$X".json && rm "$X"
+done
